f13bd98f09bb32ad3003f932b68402fbf406f460
Neha Patel <neha.r.p@gmail.com>
2019-07-18 16:37:42 -0700

    WIP header: changed background to dark blue and created a white box for header

--- src/Components/header.js
+++ src/Components/header.js
@@ -4,6 +4,13 @@ import styled from "styled-components";
 const HeaderH1 = styled.h1`
     font-size: 3em;
     margin: 20px auto;
+    color: navy;
+    max-width: 800px;
+    box-sizing: border-box;
+    background: white;
+    border-radius: 20px;
+    padding:20px 0;
+    
 `
 
 export default function Header(){
